f0f0e2f4f408fa0d292c8aa0c4b11557824d5fe2
Zaim Irfan <zaim8797@gmail.com>
2025-03-03 10:33:23 +0800

    App created, Route Parameters, Nuxt Link, Layouts

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus odit, ratione saepe nemo illo delectus eum, sunt qui voluptates est quas numquam beatae distinctio nesciunt officia voluptatem a esse? Et.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus odit, ratione saepe nemo illo delectus eum, sunt qui voluptates est quas numquam beatae distinctio nesciunt officia voluptatem a esse? Et.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+div {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;    
+}
+
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Product detail for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptates mollitia officia dolores cupiditate molestiae, consequatur voluptatem itaque, natus, saepe minima culpa animi nostrum a beatae? Velit aut quos dicta?</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+
+    definePageMeta({
+        layout: 'products'
+    })  
+</script>
+
+<style scoped>
+
+</style>
